1dbccaaf5567c14c197fcb398e314f5720ae9ff7
Svetlana <iilsojar@gmail.com>
2022-10-30 12:19:18 +0100

    напишите программу, которая на вход принимает число и выдет его квадрат

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая на вход принимает число и выдет его квадрат 
+//(число, умноженное на само себя)
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+//решение: 1. на вход принимавет число
+//         2. выдает его квадрат
+//         3. вывод результата и его проверка 
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// double number1 = 4.454;
+// double number1 = Convert.ToDouble(Console.ReadLine();
+// string str = "буквы цифры символы, строка любых символов";
+// string str1 = Console.ReadLine();
+
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square =  number *  number;
+Console.WriteLine($"квадрат числа {number} = {square}");
