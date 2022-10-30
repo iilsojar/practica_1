7b1fac10d5b077627bf728ed7d17fc658ffe11ca
Svetlana <iilsojar@gmail.com>
2022-10-30 13:01:24 +0100

    добавила второй вариант решения задачи - через еще одну переменную и сравнение

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,15 +1,29 @@
 ﻿//напишите программу, которая на вход принимает два числа и проверяет,
 //является ли первое число квадратом второго
 
+// Console.Write("введите первое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите второе число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// if (number == number1 * number1)
+// {
+//     Console.WriteLine($"первое число {number} является квадратом числа {number1}");
+// }
+// else
+// {
+//     Console.WriteLine($" число {number} не является квадратом числа {number1}");
+// }
+
 Console.Write("введите первое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("введите второе число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-if (number == number1 * number1)
+int b = Convert.ToInt32(Console.ReadLine());
+int c = b * b;
+if(a == c)
 {
-    Console.WriteLine($"первое число {number} является квадратом числа {number1}");
+    Console.WriteLine($"первое число {a} является квадратом числа {b}");
 }
 else
 {
-    Console.WriteLine($" число {number} не является квадратом числа {number1}");
+    Console.WriteLine($" число {a} не является квадратом числа {b}");
 }
