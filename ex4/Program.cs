74e93ec778385ae5e7640a0a068caf822c5db154
Svetlana <iilsojar@gmail.com>
2022-10-31 11:39:37 +0100

    напишите программу, которая на вход принимает число N и на выходе показывает все целые числа от -N до N

---
+++ ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿//напишите программу, которая на вход принимает число N и на выходе показывает все целые числа от -N до N
+//4
+//2
+
+Console.Write("введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = -number;
+while(a <= number)
+{
+    Console.Write($"{a} ");
+    a++;
+} 
+
