8624febee5f19bc3d4a90a3d1f559fa623b0ff4c
Svetlana <iilsojar@gmail.com>
2022-11-06 18:15:12 +0100

    решение задачи на поиск числа кратного 7 и 23 с помощью метода и без него

---
+++ ex15/Program.cs
@@ -0,0 +1,47 @@
+﻿//напишите программу, которая на вход принимает число и проверяет кратно ли оно 7 и 23
+//14-нет
+//46 - нет
+//161 - да
+
+// Console.Clear();
+// Console.Write("введите число-знаменатель: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("введите первое число-делитель: "); //7
+// int num1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.Write("введите второе число-делитель: "); //23
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = num % num1;
+// int result1 = num % num2;
+// if ((result == 0) && (result1 == 0))
+// {
+//     Console.WriteLine("YES");
+// }
+// else
+// {
+//     Console.WriteLine("NO");
+// }
+
+int Check(int check, int arg1, int arg2)
+{
+    int result = 1;
+    if((check % arg1 == 0) && (check % arg2 == 0)) result = 0;
+    return result;
+}
+
+Console.Write("введите число-знаменатель: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите первое число-делитель: "); //7
+int num1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("введите второе число-делитель: "); //23
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result1 = Check(num, num1, num2);
+
+if(result1 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
