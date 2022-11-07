8005bced17c22b8752d2ff4c0d8303753355a965
Svetlana <iilsojar@gmail.com>
2022-11-07 16:36:11 +0100

    cортировка массива по мин числу

---
+++ ex17/Program.cs
@@ -0,0 +1,39 @@
+﻿//сортировка массива по мин числу
+//1. найти позицию минимального элемента,
+//2. произвести обмен мин значения с неотсортированной частью массива,
+//3. повторять пока есть неотсортированные части массива
+
+Console.Clear();
+int[] arr = {1, 4, 6, 9, 5, 3, 7, 8, 1, 1};
+
+void PrintArray( int[] array)
+{
+    int count = array.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for(int j = i + 1; j < array.Length; j++)
+        {
+        if(array[j] < array[minPosition]) minPosition = j;
+        }
+    
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+PrintArray(arr);
+
+SelectionSort(arr);
+PrintArray(arr);
