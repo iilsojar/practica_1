edefc8177f919c33b0e29dc6039e0882b6b5812e
Svetlana <iilsojar@gmail.com>
2022-11-08 15:06:05 +0100

    ex16

---
+++ ex21/Program.cs
@@ -0,0 +1,15 @@
+﻿//напишите программу, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+Console.Write("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+while(a <= number)
+{
+    Console.WriteLine($"{a} ");
+    a++;
+}
+
+
+
